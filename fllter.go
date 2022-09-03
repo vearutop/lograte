f76ba7e88e6ee8e6bd539ae178e3cf8429270d8a
Viacheslav Poturaev <vearutop@gmail.com>
2022-09-03 10:26:40 +0200

    Add top lines aggregator

---
+++ fllter.go
@@ -0,0 +1,56 @@
+package main
+
+// filterAlphanumeric replaces a-zA-Z_-% sequences that have at least one digit with X.
+// Does not allocate, uses original slice.
+func filterAlphanumeric(data []byte, l int) []byte {
+	hasDigit := false
+	wordStart := -1
+
+	res := data[:0]
+	i := 0
+
+	for i = 0; i < len(data); i++ {
+		c := data[i]
+		isAlpha := false
+
+		switch {
+		case c >= 'a' && c <= 'z':
+			isAlpha = true
+		case c >= 'A' && c <= 'Z':
+			isAlpha = true
+		case c >= '0' && c <= '9':
+			isAlpha = true
+			hasDigit = true
+		case c == '-', c == '_', c == '%':
+			isAlpha = true
+		}
+
+		// Finish current word.
+		if wordStart >= 0 && !isAlpha {
+			if hasDigit {
+				res = append(res, 'X')
+			} else {
+				res = append(res, data[wordStart:i]...)
+			}
+
+			wordStart = -1
+			hasDigit = false
+		}
+
+		if wordStart == -1 {
+			if isAlpha {
+				// Starting new word.
+				wordStart = i
+			} else {
+				// Adding current char.
+				res = append(res, c)
+			}
+		}
+
+		if l > 0 && len(res) >= l {
+			return res
+		}
+	}
+
+	return res
+}
--- main.go
+++ main.go
@@ -2,88 +2,139 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"github.com/cespare/xxhash/v2"
 	"os"
-	"regexp"
 	"sort"
 	"time"
+
+	"github.com/bool64/dev/version"
+	"github.com/cespare/xxhash/v2"
 )
 
 func main() {
-	re := regexp.MustCompile(`([\w_-]{1,}\d{1,}[\w_-]{1,})+|([\w_-]{1,}\d{1,})+|(\d{1,}[\w_-]{1,})+|([\d])+`)
+	var (
+		buckets  int
+		top      int
+		length   int
+		interval time.Duration
+		ver      bool
+		bySize   bool
+		lineBuf  int
+	)
+
+	flag.IntVar(&buckets, "buckets", 500, "max number of buckets to track filtered messages")
+	flag.IntVar(&top, "top", 0, "show top filtered messages ordered by rate")
+	flag.IntVar(&length, "len", 120, "limit message length")
+	flag.DurationVar(&interval, "t", time.Second, "reporting interval")
+	flag.BoolVar(&bySize, "by-size", false, "order messages by size instead of count")
+	flag.BoolVar(&ver, "version", false, "print version and exit")
+	flag.IntVar(&lineBuf, "line-buf", 1e7, "line token buffer size")
+
+	flag.Parse()
+
+	if ver {
+		fmt.Println(version.Info().Version)
+		return
+	}
+
 	d := xxhash.New()
 
 	scanner := bufio.NewScanner(os.Stdin)
+	buf := make([]byte, lineBuf)
+	scanner.Buffer(buf, len(buf))
+
 	cnt := 0
+	byteCnt := 0
+
 	start := time.Now()
 	lastReport := start
 
-	counts := map[uint64]uint64{}
+	counts := map[uint64]int{}
+	byteCounts := map[uint64]int{}
 	samples := map[uint64]string{0: "Other"}
 	entries := make([]entry, 0, buckets)
 
 	for scanner.Scan() {
-		cnt++
-
 		line := scanner.Bytes()
-		line = head(line, 100)
-		filtered := re.ReplaceAll(line, []byte("X"))
-		d.Reset()
-		_, _ = d.Write(filtered)
-		h := d.Sum64()
+		cnt++
+		byteCnt += len(line)
 
-		if counts[h] == 0 {
-			if len(counts) > 100 {
-				h = 0
-			} else {
-				//samples[h] = string(head(scanner.Bytes(), 200))
-				samples[h] = string(head(filtered, 200))
-			}
-		}
-		counts[h]++
-
-		if time.Since(lastReport) > time.Second {
+		if time.Since(lastReport) > interval {
 			lastReport = time.Now()
+			ela := time.Since(start).Seconds()
+			lps := float64(cnt) / ela
+			MBps := float64(byteCnt) / (ela * 1024 * 1024)
+
+			fmt.Println(scanner.Text())
+			fmt.Printf("%d lines since %s, %.1f per second, %.1f MB/s, %d B/avg\n",
+				cnt, start.Format(time.Stamp), lps, MBps, byteCnt/cnt)
+
+			if top <= 0 {
+				continue
+			}
 
 			entries = entries[:0]
 			for h, c := range counts {
 				entries = append(entries, entry{
-					hash: h,
-					cnt:  c,
+					hash:  h,
+					cnt:   c,
+					bytes: byteCounts[h],
 				})
 			}
 
-			sort.Slice(entries, func(i, j int) bool {
-				return entries[i].cnt > entries[j].cnt
-			})
-
-			fmt.Println(scanner.Text())
-			fmt.Printf("%d lines since %s, %.1f per second\n", cnt, start.Format(time.Stamp), float64(cnt)/time.Since(start).Seconds())
-
-			fmt.Println("Top 10 -------------------------")
-			for _, e := range entries[0:10] {
-				fmt.Printf("%d lines, %.1f lps: %s\n", e.cnt, float64(e.cnt)/time.Since(start).Seconds(), samples[e.hash])
+			if bySize {
+				sort.Slice(entries, func(i, j int) bool {
+					return entries[i].bytes > entries[j].bytes
+				})
+			} else {
+				sort.Slice(entries, func(i, j int) bool {
+					return entries[i].cnt > entries[j].cnt
+				})
 			}
-			fmt.Println("-------------------------")
+
+			fmt.Printf("------ Top %d -------------------------\n", top)
+			if len(entries) > top {
+				entries = entries[0:top]
+			}
+
+			for _, e := range entries {
+				cntPercent := 100 * float64(e.cnt) / float64(cnt)
+				bytesPercent := 100 * float64(e.bytes) / float64(byteCnt)
+				lps = float64(e.cnt) / ela
+				MBps = float64(byteCounts[e.hash]) / (ela * 1024 * 1024)
+				fmt.Printf("%d lines, %.1f lps (%.1f%%), %.1f MB/s (%.1f%%), %d B/avg: %s\n",
+					e.cnt, lps, cntPercent, MBps, bytesPercent, e.bytes/e.cnt, samples[e.hash])
+			}
+			fmt.Printf("---------------------------------------\n\n")
+		}
+
+		if top > 0 {
+			filtered := filterAlphanumeric(line, length)
+			d.Reset()
+			_, _ = d.Write(filtered)
+			h := d.Sum64()
+
+			if counts[h] == 0 {
+				if len(counts) > buckets {
+					h = 0
+				} else {
+					samples[h] = string(filtered)
+				}
+			}
+
+			counts[h]++
+			byteCounts[h] += len(line)
 		}
 	}
-}
 
-// update_worker -> update_consumer.go -> queryValues method
-
-func head(d []byte, l int) []byte {
-	if len(d) < l {
-		return d
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Scan error:", err.Error())
 	}
-
-	return d[0:l]
 }
-
-const buckets = 1000
 
 type entry struct {
 	hash  uint64
-	cnt   uint64
-	bytes uint64
+	cnt   int
+	bytes int
 }
