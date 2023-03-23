99bb6f3a68a3d347c13741d09052c5cac126390b
Viacheslav Poturaev <vearutop@gmail.com>
2023-03-23 17:47:57 +0100

    Extend filtering to sequences of vowels/consonants (#5)

--- fllter.go
+++ fllter.go
@@ -1,24 +1,42 @@
 package main
 
-// filterAlphanumeric replaces a-zA-Z_-% sequences that have at least one digit with X.
+// filterDynamic replaces a-zA-Z_-% sequences that have at least one digit or 5+ consecutive consolants/vowels with X.
 // Does not allocate, uses original slice.
-func filterAlphanumeric(data []byte, l int) []byte {
+func filterDynamic(data []byte, l int) []byte {
 	hasDigit := false
 	wordStart := -1
+	maxConsecutive := 0
+	consecutive := 0
 
 	res := data[:0]
 
-	var i int
+	var (
+		i            int
+		prevCharType byte
+	)
 
 	for i = 0; i < len(data); i++ {
 		c := data[i]
 
 		isAlpha := false
+		charType := byte(0)
 
 		switch {
 		case c >= 'a' && c <= 'z':
+			if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' || c == 'w' {
+				charType = 'v' // Vowel.
+			} else {
+				charType = 'c' // Consonant.
+			}
+
 			isAlpha = true
 		case c >= 'A' && c <= 'Z':
+			if c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y' || c == 'W' {
+				charType = 'v'
+			} else {
+				charType = 'c'
+			}
+
 			isAlpha = true
 		case c >= '0' && c <= '9':
 			isAlpha = true
@@ -27,9 +45,20 @@ func filterAlphanumeric(data []byte, l int) []byte {
 			isAlpha = true
 		}
 
+		if charType == prevCharType {
+			consecutive++
+		} else {
+			if consecutive > maxConsecutive {
+				maxConsecutive = consecutive
+			}
+
+			prevCharType = charType
+			consecutive = 1
+		}
+
 		// Finish current word.
 		if wordStart >= 0 && !isAlpha {
-			if hasDigit {
+			if hasDigit || maxConsecutive > 4 {
 				res = append(res, 'X')
 			} else {
 				res = append(res, data[wordStart:i]...)
@@ -37,6 +66,8 @@ func filterAlphanumeric(data []byte, l int) []byte {
 
 			wordStart = -1
 			hasDigit = false
+			maxConsecutive = 0
+			consecutive = 0
 		}
 
 		if wordStart == -1 {
