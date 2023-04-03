129aa06212d092f705165812d109ff87e8294b9b
Viacheslav Poturaev <vearutop@gmail.com>
2023-04-03 20:03:36 +0200

    Add parse time for static logs

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"runtime/pprof"
 	"sort"
 	"time"
@@ -17,14 +18,16 @@ import (
 
 func main() {
 	var (
-		buckets    int
-		top        int
-		length     int
-		interval   time.Duration
-		ver        bool
-		bySize     bool
-		lineBuf    int
-		cpuProfile string
+		buckets         int
+		top             int
+		length          int
+		interval        time.Duration
+		ver             bool
+		bySize          bool
+		lineBuf         int
+		cpuProfile      string
+		parseTimeRegex  string
+		parseTimeFormat string
 	)
 
 	flag.IntVar(&buckets, "buckets", 500, "max number of buckets to track filtered messages")
@@ -34,6 +37,8 @@ func main() {
 	flag.BoolVar(&bySize, "by-size", false, "order messages by size instead of count")
 	flag.BoolVar(&ver, "version", false, "print version and exit")
 	flag.IntVar(&lineBuf, "line-buf", 1e7, "line token buffer size")
+	flag.StringVar(&parseTimeRegex, "parse-time-regex", "", "regex to parse time value from log line")
+	flag.StringVar(&parseTimeFormat, "parse-time-format", "", "format to parse time from log line")
 	flag.StringVar(&cpuProfile, "dbg-cpu-prof", "", "write first 10 seconds of CPU profile to file")
 
 	flag.Parse()
@@ -71,11 +76,79 @@ func main() {
 
 	start := time.Now()
 	lastReport := start
+	lastTs := start
 
 	counts := map[uint64]int{}
 	byteCounts := map[uint64]int{}
 	samples := map[uint64]string{0: "Other"}
 	entries := make([]entry, 0, buckets)
+
+	var timeRegex *regexp.Regexp
+	if parseTimeRegex != "" {
+		timeRegex = regexp.MustCompile(parseTimeRegex)
+		start = time.Time{}
+	}
+
+	report := func() {
+		lastReport = time.Now()
+
+		if timeRegex == nil {
+			lastTs = lastReport
+		}
+
+		ela := lastTs.Sub(start).Seconds()
+		lps := float64(cnt) / ela
+		MBps := float64(byteCnt) / (ela * 1024 * 1024)
+
+		fmt.Println(scanner.Text())
+		fmt.Printf("%d lines since %s, %.1f per second, %.1f MB/s, %d B/avg\n",
+			cnt, start.Format(time.Stamp), lps, MBps, byteCnt/cnt)
+
+		if top <= 0 {
+			return
+		}
+
+		entries = entries[:0]
+		for h, c := range counts {
+			entries = append(entries, entry{
+				hash:  h,
+				cnt:   c,
+				bytes: byteCounts[h],
+			})
+		}
+
+		if bySize {
+			sort.Slice(entries, func(i, j int) bool {
+				return entries[i].bytes > entries[j].bytes
+			})
+		} else {
+			sort.Slice(entries, func(i, j int) bool {
+				return entries[i].cnt > entries[j].cnt
+			})
+		}
+
+		fmt.Printf("------ Top %d -------------------------\n", top)
+
+		if len(entries) > top {
+			entries = entries[0:top]
+		}
+
+		for _, e := range entries {
+			cntPercent := 100 * float64(e.cnt) / float64(cnt)
+			bytesPercent := 100 * float64(e.bytes) / float64(byteCnt)
+			lps = float64(e.cnt) / ela
+			MBps = float64(byteCounts[e.hash]) / (ela * 1024 * 1024)
+			fmt.Printf("%d lines, %.1f lps (%.1f%%), %.1f MB/s (%.1f%%), %d B/avg: %s\n",
+				e.cnt, lps, cntPercent, MBps, bytesPercent, e.bytes/e.cnt, samples[e.hash])
+		}
+
+		fmt.Printf("---------------------------------------\n\n")
+	}
+
+	var (
+		ts  time.Time
+		err error
+	)
 
 	for scanner.Scan() {
 		line := scanner.Bytes()
@@ -83,55 +156,28 @@ func main() {
 
 		byteCnt += len(line)
 
+		if timeRegex != nil {
+			matches := timeRegex.FindSubmatch(line)
+			if len(matches) == 0 {
+				panic("no matches for timestamp regex")
+			}
+
+			ts, err = time.Parse(parseTimeFormat, string(matches[1]))
+			if err != nil {
+				panic(err)
+			}
+
+			if start.IsZero() {
+				start = ts
+			}
+
+			lastTs = ts
+		} else {
+			ts = time.Now()
+		}
+
 		if time.Since(lastReport) > interval {
-			lastReport = time.Now()
-			ela := time.Since(start).Seconds()
-			lps := float64(cnt) / ela
-			MBps := float64(byteCnt) / (ela * 1024 * 1024)
-
-			fmt.Println(scanner.Text())
-			fmt.Printf("%d lines since %s, %.1f per second, %.1f MB/s, %d B/avg\n",
-				cnt, start.Format(time.Stamp), lps, MBps, byteCnt/cnt)
-
-			if top <= 0 {
-				continue
-			}
-
-			entries = entries[:0]
-			for h, c := range counts {
-				entries = append(entries, entry{
-					hash:  h,
-					cnt:   c,
-					bytes: byteCounts[h],
-				})
-			}
-
-			if bySize {
-				sort.Slice(entries, func(i, j int) bool {
-					return entries[i].bytes > entries[j].bytes
-				})
-			} else {
-				sort.Slice(entries, func(i, j int) bool {
-					return entries[i].cnt > entries[j].cnt
-				})
-			}
-
-			fmt.Printf("------ Top %d -------------------------\n", top)
-
-			if len(entries) > top {
-				entries = entries[0:top]
-			}
-
-			for _, e := range entries {
-				cntPercent := 100 * float64(e.cnt) / float64(cnt)
-				bytesPercent := 100 * float64(e.bytes) / float64(byteCnt)
-				lps = float64(e.cnt) / ela
-				MBps = float64(byteCounts[e.hash]) / (ela * 1024 * 1024)
-				fmt.Printf("%d lines, %.1f lps (%.1f%%), %.1f MB/s (%.1f%%), %d B/avg: %s\n",
-					e.cnt, lps, cntPercent, MBps, bytesPercent, e.bytes/e.cnt, samples[e.hash])
-			}
-
-			fmt.Printf("---------------------------------------\n\n")
+			report()
 		}
 
 		if top > 0 {
@@ -159,6 +205,8 @@ func main() {
 			byteCounts[h] += len(line)
 		}
 	}
+
+	report()
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Scan error:", err.Error())
